23825cc5e9e1a78cb44bdc5f528aab0504844cc9
mrbubbles-src <feaver5683@gmail.com>
2023-08-09 11:45:50 +0200

    created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./scss/App.scss";
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>hi</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
