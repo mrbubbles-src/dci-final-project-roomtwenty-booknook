91035ea22912b22708dcd41509211e38e204c39f
mrbubbles-src <feaver5683@gmail.com>
2023-08-10 11:22:39 +0200

    erstellt

---
+++ frontend/src/pages/SharedLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+// import Navbar from "../components/Navbar/Navbar";
+// import MenuBar from "../components/MenuBar/MenuBar";
+// import Footer from "../components/Footer/Footer";
+
+const SharedLayout = () => {
+    return (
+        <>
+            {/* <Navbar /> // transparenter hintergrund; farbig ab Desktop größe */}
+            <main className="main-container">
+                <Outlet />
+            </main>
+            {/* <MenuBar /> // ausgeschaltet ab Desktop größe */}
+            {/* <Footer /> */}
+        </>
+    );
+};
+
+export default SharedLayout;
