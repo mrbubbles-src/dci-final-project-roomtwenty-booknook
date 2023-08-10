32e75254bc567d32ffa81769e1c90780d0d1ef14
mrbubbles-src <feaver5683@gmail.com>
2023-08-10 11:30:57 +0200

    routes rohling erstellt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BookNookProvider from "./context/BookNookProvider";
 import "./scss/App.scss";
+import SharedLayout from "./pages/SharedLayout";
+import NotFound from "./pages/NotFound/NotFound";
+import ScrollToTop from "./helpers/ScrollToTop";
 
 function App() {
     return (
-        <>
-            <div>
-                <h1>hi</h1>
-            </div>
-        </>
+        <BookNookProvider>
+            <BrowserRouter>
+                <ScrollToTop />
+                <Routes>
+                    <Route path="/" element={<SharedLayout />}>
+                        {/* die jeweiligen pages routes hier zwische rein */}
+
+                        {/* die jeweiligen pages routes hier zwische rein */}
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </BookNookProvider>
     );
 }
 
