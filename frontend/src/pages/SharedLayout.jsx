8dda72c396c470d37bf384d72b36933ae806778d
mrbubbles-src <feaver5683@gmail.com>
2023-08-10 11:39:40 +0200

    nevbar, menubar und footer wieder einkommentiert

--- frontend/src/pages/SharedLayout.jsx
+++ frontend/src/pages/SharedLayout.jsx
@@ -1,17 +1,17 @@
 import { Outlet } from "react-router-dom";
-// import Navbar from "../components/Navbar/Navbar";
-// import MenuBar from "../components/MenuBar/MenuBar";
-// import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import MenuBar from "../components/MenuBar/MenuBar";
+import Footer from "../components/Footer/Footer";
 
 const SharedLayout = () => {
     return (
         <>
-            {/* <Navbar /> // transparenter hintergrund; farbig ab Desktop größe */}
+            <Navbar /> // transparenter hintergrund; farbig ab Desktop größe
             <main className="main-container">
                 <Outlet />
             </main>
-            {/* <MenuBar /> // ausgeschaltet ab Desktop größe */}
-            {/* <Footer /> */}
+            <MenuBar /> // ausgeschaltet ab Desktop größe
+            <Footer />
         </>
     );
 };
