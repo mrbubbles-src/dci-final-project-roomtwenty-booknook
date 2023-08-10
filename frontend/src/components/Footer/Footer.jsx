42c27f83952fa5415923168faa995920e3299804
mrbubbles-src <feaver5683@gmail.com>
2023-08-10 11:39:15 +0200

    rohling erstellt

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <p>hallo ich bin ein footer</p>
+        </footer>
+    );
+};
+
+export default Footer;
