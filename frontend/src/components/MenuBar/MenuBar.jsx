7ba822dadadae8fc28b46cfcd3f3971e30c4acac
mrbubbles-src <feaver5683@gmail.com>
2023-08-10 11:39:25 +0200

    rohling erstellt

---
+++ frontend/src/components/MenuBar/MenuBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuBar = () => {
+    return (
+        <div className="menubar-container">
+            <p>hallo ich bin die MenuBar</p>
+        </div>
+    );
+};
+
+export default MenuBar;
