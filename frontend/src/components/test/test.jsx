867d003d23c958e64a531dc1c778c36d3b740d54
mrbubbles-src <feaver5683@gmail.com>
2023-08-14 19:46:15 +0200

    removed api key and made it secure :E

--- frontend/src/components/test/test.jsx
+++ frontend/src/components/test/test.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "dotenv/config";
+
+const api_key = process.env.BOOKS_API_KEY;
 
 const Test = () => {
     const [bookData, setBookData] = useState({});
@@ -7,7 +10,7 @@ const Test = () => {
     useEffect(() => {
         async function fetchData() {
             const res = await fetch(
-                "https://www.googleapis.com/books/v1/volumes?q=harrypotter&key=AIzaSyDMmyHDYPAYZvCCgk40pWRufmxv2bZ_IOs"
+                `https://www.googleapis.com/books/v1/volumes?q=harrypotter&key=${api_key}`
             );
             const data = await res.json();
             // console.log("data", data);
