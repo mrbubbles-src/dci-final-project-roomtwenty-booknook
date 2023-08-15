4a228a5823f5181160165d36125636ece85f7e58
MuhammedTinmaz <muhammed.tinmaz@dci.education>
2023-08-15 15:15:04 +0200

    first commit

--- frontend/src/pages/SharedLayout.jsx
+++ frontend/src/pages/SharedLayout.jsx
@@ -6,12 +6,12 @@ import Footer from "../components/Footer/Footer";
 const SharedLayout = () => {
     return (
         <>
-            <Navbar /> // transparenter hintergrund; farbig ab Desktop größe
+            {/* <Navbar />  transparenter hintergrund; farbig ab Desktop größe */}
             <main className="main-container">
                 <Outlet />
             </main>
-            <MenuBar /> // ausgeschaltet ab Desktop größe
-            <Footer />
+            <MenuBar /> {/*  ausgeschaltet ab Desktop größe */}
+            {/* <Footer /> */}
         </>
     );
 };
