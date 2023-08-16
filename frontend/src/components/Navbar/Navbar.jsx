4599c3160f2e3c3e7f130aec66234399c44a0bf2
metin <metinmete.arsu@dci.education>
2023-08-16 13:14:00 +0200

    Add: Modal Logic for Login Button

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./navbar.scss";
+import { useState } from "react";
+import LoginForm from "../../components/LoginForm";
 
 const Navbar = () => {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+
+    const handleCloseModal = () => {
+        setShowLoginModal(false);
+    };
+
+    const handleLoginClick = () => {
+        setShowLoginModal(true);
+    };
+
+    const handleLoginFormSubmit = (formData) => {
+        console.log("Username:", formData.username);
+        console.log("Password:", formData);
+        setShowLoginModal(false);
+    };
+
     return (
         <nav className='navbar-container'>
             <div className='logo-container'>
@@ -9,6 +27,17 @@ const Navbar = () => {
             </div>
             <div className='btn-container'>
                 <button className='btn-login'>Login</button>
+                {showLoginModal && (
+                    <div className='modal-overlay'>
+                        <div className='modal'>
+                            <h2>Login</h2>
+                            <LoginForm
+                                onSubmit={handleLoginFormSubmit}
+                                onCancel={handleCloseModal}
+                            />
+                        </div>{" "}
+                    </div>
+                )}
             </div>
         </nav>
     );
