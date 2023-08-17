272e9d7c0b624b68e128c923225f39c54321c9a4
mrbubbles-src <feaver5683@gmail.com>
2023-08-17 19:59:39 +0200

    added default search term again

--- frontend/src/context/BookNookProvider.jsx
+++ frontend/src/context/BookNookProvider.jsx
@@ -6,7 +6,7 @@ const BookNookProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(null);
     // buch suche spezifischer context start
     const [userInput, setUserInput] = useState("");
-    const [searchTerm, setSearchTerm] = useState("Harry Potter");
+    const [searchTerm, setSearchTerm] = useState("harry potter");
     const [bookData, setBookData] = useState({});
     const [searchReadMore, setsearchReadMore] = useState(false);
 
