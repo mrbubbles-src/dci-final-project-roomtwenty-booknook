3c60626736ac27a0fba19f1c9524a02ca302e7b1
mrbubbles-src <feaver5683@gmail.com>
2023-08-18 11:57:41 +0200

    erstellt

---
+++ frontend/src/pages/Search/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchForm from "../../components/SearchForm/SearchForm";
+import BookSearch from "../../components/BookSearch/BookSearch";
+
+const Search = () => {
+    return (
+        <>
+            <SearchForm />
+            <BookSearch />
+        </>
+    );
+};
+
+export default Search;
