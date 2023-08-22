d2ead4a67b9da75ecf3521cbcde817953ac6fd45
mrbubbles-src <feaver5683@gmail.com>
2023-08-22 14:28:41 +0200

    most info shown implemented

--- frontend/src/pages/SingleBookDetails/SingleBookDetails.jsx
+++ frontend/src/pages/SingleBookDetails/SingleBookDetails.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import NoImage from "../../../public/images/various/no-image.png";
 import "./singleBookDetails.scss";
+import { BookNookContext } from "../../context/BookNookProvider";
 
 const SingleBookDetails = () => {
     const { id } = useParams();
     const [isSingleBookLoading, setIsSingleBookLoading] = useState(true);
     const [singleBookData, setSingleBookData] = useState({});
+    const { bookData } = useContext(BookNookContext);
     useEffect(() => {
         async function fetchData() {
             const response = await fetch(
@@ -24,6 +26,7 @@ const SingleBookDetails = () => {
     }
     const {
         title,
+        subtitle,
         authors,
         publisher,
         publishedDate,
@@ -31,45 +34,116 @@ const SingleBookDetails = () => {
         pageCount,
         categories,
         averageRating,
-        ratingCount,
+        ratingsCount,
         previewLink,
+        industryIdentifiers,
     } = singleBookData.volumeInfo || {};
-    const { type, identifier } =
-        singleBookData.volumeInfo.industryIdentifiers || {}; // isbn 10+13
-    const { smallThumbnail, thumbnail, small, medium, large, extraLarge } =
+    // const { type, identifier } =
+    //     singleBookData.volumeInfo.industryIdentifiers || {}; // isbn 10+13
+    const { smallThumbnail, thumbnail, medium, extraLarge } =
         singleBookData.volumeInfo.imageLinks || {};
+
+    let isbn10Title = "";
+    let isbn10Number = "";
+    let isbn13Title = "";
+    let isbn13Number = "";
+
+    if (industryIdentifiers) {
+        for (const industryIdentifier of industryIdentifiers) {
+            if (
+                industryIdentifier.type === "ISBN_10" ||
+                industryIdentifier.type.includes("10")
+            ) {
+                isbn10Title = industryIdentifier.type.replace("_", "-");
+                isbn10Number = industryIdentifier.identifier;
+            } else {
+                isbn13Title = industryIdentifier.type.replace("_", "-");
+                isbn13Number = industryIdentifier.identifier;
+            }
+        }
+    }
 
     const genres = [];
 
-    categories.forEach((category) => {
-        const tempCat = category.split(" / ");
-        tempCat.forEach((element) => {
-            if (!genres.includes(element)) {
-                genres.push(element);
-            }
+    categories &&
+        categories.forEach((category) => {
+            const tempCat = category.split(" / ");
+            tempCat.forEach((element) => {
+                if (!genres.includes(element)) {
+                    genres.push(element);
+                }
+            });
         });
-    });
 
     return (
         <div className="single-book-container">
-            <h2 className="single-book-title">{title}</h2>
+            <h2 className="single-book-title">
+                {title || "Unbekannter Titel"}
+            </h2>
+            {subtitle ? (
+                <h3 className="single-book-subtitle">{subtitle}</h3>
+            ) : null}
+            <h5 className="single-book-author">
+                von {authors || "Unbekannter Autor"}
+            </h5>
             <img
                 src={medium || thumbnail || smallThumbnail || NoImage}
                 alt={`${title} cover`}
             />
-            <p>{description}</p>
-            <p>
+            <p className="single-book-description">
+                {description?.replace("<p>", "").replace("</p>", "")}
+            </p>
+            <p className="single-book-genre-container">
                 Genres:{" "}
                 {genres && genres.length >= 1
                     ? genres.map((category, index) => {
                           return (
-                              <span className="genre" key={index}>
+                              <span className="single-book-genre" key={index}>
                                   {category}
                               </span>
                           );
                       })
                     : "keine Genres bekannt"}
             </p>
+            <section className="single-book-info-section">
+                <h5 className="single-book-info-section-title">
+                    Buchinformationen:
+                </h5>
+                <p className="single-book-pagecount-container">
+                    <span className="single-book-pagecount-count">
+                        {pageCount || "Unbekannte anzahl"}
+                    </span>{" "}
+                    Seiten
+                </p>
+                <p className="single-book-publisher-container">
+                    Verlag:{" "}
+                    <span className="single-book-publisher">{publisher}</span>
+                </p>
+                <p className="single-book-first-published-container">
+                    Erstveröffentlichung:{" "}
+                    <span className="single-book-first-published-date">
+                        {publishedDate || "Unbekanntes Datum"}
+                    </span>
+                </p>
+                <p className="single-book-isbn">
+                    {isbn10Title || "ISBN-10"}:{" "}
+                    {isbn10Number || "Keine ISBN-10 bekannt"}
+                </p>{" "}
+                <p className="single-book-isbn">
+                    {isbn13Title || "ISBN-13"}:{" "}
+                    {isbn13Number || "Keine ISBN-13 bekannt"}
+                </p>
+                <p className="single-book-rating-container">
+                    <span className="single-book-avg-rating">
+                        Durchschnittlich bewertet mit{" "}
+                        {averageRating || "unbekannter anzahl von"} 🌟
+                    </span>{" "}
+                    bei{" "}
+                    <span className="single-book-ratingcount">
+                        {ratingsCount || "unbekannter anzahl"} Bewertungen
+                    </span>
+                </p>
+            </section>
         </div>
     );
 };
