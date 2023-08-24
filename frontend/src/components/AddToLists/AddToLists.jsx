dc5f333da3e185ee20d53a6a295982ba4b6fdf21
mrbubbles-src <feaver5683@gmail.com>
2023-08-24 14:33:36 +0200

    prop management

--- frontend/src/components/AddToLists/AddToLists.jsx
+++ frontend/src/components/AddToLists/AddToLists.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-const AddToLists = ({}) => {
+const AddToLists = ({ onButtonClick }) => {
+    const handleButtonClick = (url) => {
+        onButtonClick(url);
+    };
     return (
         <div>
             <div className="book-actions-add-to-lists-container">
-                <button className="book-actions-add-to-lists want-to-read">
+                <button
+                    className="book-actions-add-to-lists want-to-read"
+                    onClick={() => handleButtonClick("url string here")}
+                >
                     Möchte ich lesen
                 </button>
                 <button className="book-actions-add-to-lists-dropdown">
