22efc55caf66d8461b5ccbbde93f994f2e001fd9
mrbubbles-src <feaver5683@gmail.com>
2023-08-24 13:36:46 +0200

    base created

---
+++ frontend/src/components/AddToLists/AddToLists.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+const AddToLists = () => {
+    return (
+        <div>
+            <div className="book-actions-add-to-lists-container">
+                <button className="book-actions-add-to-lists want-to-read">
+                    Möchte ich lesen
+                </button>
+                <button className="book-actions-add-to-lists-dropdown">
+                    {<FontAwesomeIcon icon={faChevronDown} />}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default AddToLists;
