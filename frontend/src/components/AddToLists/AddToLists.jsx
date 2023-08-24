764f9a4bdd86379bada15a640e258d7e64ba998a
mrbubbles-src <feaver5683@gmail.com>
2023-08-24 14:23:09 +0200

    wording, props handling

--- frontend/src/components/AddToLists/AddToLists.jsx
+++ frontend/src/components/AddToLists/AddToLists.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-const AddToLists = () => {
+const AddToLists = ({}) => {
     return (
         <div>
             <div className="book-actions-add-to-lists-container">
