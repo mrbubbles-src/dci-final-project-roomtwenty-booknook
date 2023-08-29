feb6e1e82094a390fe00293e2ba3d08ec8af6d8d
metin <metinmete.arsu@dci.education>
2023-08-29 11:37:49 +0200

    login fixed, port fixed

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
-import useAuth from "../../customhooks/auth"
+import useAuth from "../../customhooks/auth";
 import "./loginform.scss";
 
 const LoginForm = ({ onClose }) => {
