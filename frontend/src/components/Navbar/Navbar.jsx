0a7da076219780b6b32fe009af93051a60337831
mrbubbles-src <feaver5683@gmail.com>
2023-08-30 13:25:52 +0200

    überflüssiges entfernt

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     const [showModal, setShowModal] = useState(false);
     const [isLoginVisible, setIsLoginVisible] = useState(true);
-    const [checkCookie, setCheckCookie] = useState("")
 
     const toggleForm = () => {
         setIsLoginVisible(!isLoginVisible);
@@ -23,9 +22,9 @@ const Navbar = () => {
     };
 
     return (
-        <nav className='navbar-container'>
-            <div className='logo-container'>
-                <Link to={"/"} className='logo-text'>
+        <nav className="navbar-container">
+            <div className="logo-container">
+                <Link to={"/"} className="logo-text">
                     bookNook
                 </Link>{" "}
             </div>
@@ -40,7 +39,7 @@ const Navbar = () => {
                         ) : (
                             <SignupForm onClose={handleCloseModal} />
                         )}
-                        <button className='btn-style' onClick={toggleForm}>
+                        <button className="btn-style" onClick={toggleForm}>
                             {isLoginVisible
                                 ? "Noch kein Mitglied?"
                                 : "Bereits Mitglied?"}
