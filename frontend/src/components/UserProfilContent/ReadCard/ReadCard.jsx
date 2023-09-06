2634271df96cc74e1c22ebb93a3387cdc71e8eb1
katharina-groller <Katharina.Groller@dci.education>
2023-09-06 14:19:05 +0200

    sort files and folders 1

---
+++ frontend/src/components/UserProfilContent/ReadCard/ReadCard.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./ReadCard.scss";
+const ReadCard = () => {
+    return (
+        <>
+            <p>Hat bereits gelesen</p>
+            <div className="bereits-gelesen profile-card-container">
+                <p>Hier kommen die Bücher rein</p>
+            </div>
+        </>
+    );
+};
+export default ReadCard;
--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -3,18 +3,18 @@ import LeseChallenge from "../../components/LeseChallenge/LeseChallenge";
 import "./userprofile.scss";
 import FileUpload from "../../components/FileUpload/FileUpload";
 import UserStatistic from "../../components/UserStatistic/UserStatistic";
+import LeseFortschritt from "../../components/UserProfilContent/LeseFortschritt";
 
 const UserProfile = () => {
     return (
         <>
             <h1 className="profile">Profile</h1>
             <div>
-                <UserStatistic/>
+                <UserStatistic />
                 <LeseChallenge />
-                <FileUpload/>
-            
+                <LeseFortschritt />
+                <FileUpload />
             </div>
-
         </>
     );
 };
