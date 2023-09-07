1ff9e4147b6adb181670ef3b339df32a91172be7
mrbubbles-src <feaver5683@gmail.com>
2023-09-07 10:14:06 +0200

    removed clg

--- frontend/src/components/UserStatistic/UserStatistic.jsx
+++ frontend/src/components/UserStatistic/UserStatistic.jsx
@@ -28,8 +28,8 @@ const UserStatistic = () => {
                 .then((response) => {
                     const readListData = response.data;
                     setReadListData(readListData);
-                    console.log(readListData);
-                    console.log(readListData.profilePicture);
+                    // console.log(readListData);
+                    // console.log(readListData.profilePicture);
                 })
                 .catch((error) => {
                     console.error("Something went wrong?!", error);
@@ -42,7 +42,6 @@ const UserStatistic = () => {
     }
     const serverURL = "http://localhost:3000";
     const Avatar = `${serverURL}${readListData.profilePicture}`;
-    console.log("Avatar Path", Avatar);
     return (
         <div>
             <h1>{readListData.title}</h1>
