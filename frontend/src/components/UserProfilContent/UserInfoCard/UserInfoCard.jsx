acb451e7fec5cc963eb2caa62fc9284cdcbdac4e
mrbubbles-src <feaver5683@gmail.com>
2023-09-08 11:09:35 +0200

    daten weiter an statistik übergeben

--- frontend/src/components/UserProfilContent/UserInfoCard/UserInfoCard.jsx
+++ frontend/src/components/UserProfilContent/UserInfoCard/UserInfoCard.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./UserInfoCard.scss";
 import UserStatistic from "../../UserStatistic/UserStatistic";
-const UserInfoCard = () => {
+const UserInfoCard = ({
+    wantToRead,
+    currentlyReading,
+    alreadyRead,
+    username,
+    profileImage,
+}) => {
     return (
         <div className="user-profile profile-card-container">
-            <UserStatistic />
+            <UserStatistic
+                wantToRead={wantToRead}
+                currentlyReading={currentlyReading}
+                alreadyRead={alreadyRead}
+                username={username}
+                profileImage={profileImage}
+            />
         </div>
     );
 };
