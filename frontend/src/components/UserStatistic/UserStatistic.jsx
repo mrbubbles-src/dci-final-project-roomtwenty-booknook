90d97cf10c66e60ff8c99cbeef9ac967c5b8bcd7
mrbubbles-src <feaver5683@gmail.com>
2023-09-08 11:10:03 +0200

    daten angepasst für anzeige

--- frontend/src/components/UserStatistic/UserStatistic.jsx
+++ frontend/src/components/UserStatistic/UserStatistic.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Cookies from "js-cookie";
+// import axios from "axios";
+// import Cookies from "js-cookie";
 import "./userstatistic.scss";
 
 function getPluralText(count, singularText, pluralText) {
@@ -13,60 +13,38 @@ function getPluralText(count, singularText, pluralText) {
     }
 }
 // nur /images/dateiname im backend abspeichern
-const UserStatistic = () => {
-    const [readListData, setReadListData] = useState(null);
-
-    // useEffect(() => {
-    //     const token = Cookies.get("jwtToken");
-    //     if (token) {
-    //         axios
-    //             .get("http://localhost:3000/users/getReadlist", {
-    //                 headers: {
-    //                     Authorization: `Bearer ${token}`,
-    //                 },
-    //             })
-    //             .then((response) => {
-    //                 const readListData = response.data;
-    //                 setReadListData(readListData);
-    //                 // console.log(readListData);
-    //                 // console.log(readListData.profilePicture);
-    //             })
-    //             .catch((error) => {
-    //                 console.error("Something went wrong?!", error);
-    //             });
-    //     }
-    // }, []);
-
-    if (!readListData) {
-        return <div>Lade...</div>;
-    }
-    const serverURL = "http://localhost:3000";
-    const Avatar = `${serverURL}${readListData.profilePicture}`;
+const UserStatistic = ({
+    wantToRead,
+    currentlyReading,
+    alreadyRead,
+    username,
+    profileImage,
+}) => {
     return (
         <div>
             {/* <h1>{readListData.title}</h1> */}
             <p>
                 {getPluralText(
-                    readListData.alreadyRead.length,
+                    alreadyRead.length,
                     "bereits gelesenes Buch",
                     "bereits gelesene Bücher"
                 )}
             </p>
             <p>
                 {getPluralText(
-                    readListData.currentlyReading.length,
+                    currentlyReading.length,
                     "aktuell lesendes Buch",
                     "aktuell lesende Bücher"
                 )}
             </p>
             <p>
                 {getPluralText(
-                    readListData.wantToRead.length,
+                    wantToRead.length,
                     "gewünschtes Buch",
                     "gewünschte Bücher"
                 )}
             </p>
-            <img src={Avatar} alt="avatar" width={"100px"} />
+            {/* <img src={Avatar} alt="avatar" width={"100px"} /> */}
         </div>
     );
 };
