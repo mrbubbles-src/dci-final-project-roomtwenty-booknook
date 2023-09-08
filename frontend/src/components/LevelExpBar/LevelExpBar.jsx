456c964e08ee7b6dff95f3e02ee5f913792e3619
MuhammedTinmaz <muhammed.tinmaz@dci.education>
2023-09-08 09:36:41 +0200

    kommentare erstellt

--- frontend/src/components/LevelExpBar/LevelExpBar.jsx
+++ frontend/src/components/LevelExpBar/LevelExpBar.jsx
@@ -6,7 +6,12 @@ const LevelExpBar = ({ xpProzent }) => {
     const mainDiv = {
         width: "150px",
     };
-    
+
+    const updateReadPages = async (userId, newPages) =>{
+        const user = await getUserById(userId);
+        user.readPages += newPages;
+        await user.save();
+    }    
 
     return (
         <div className="expLevel" style={mainDiv}>
