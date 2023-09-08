79e1cb12b2e216408e34b8f08832e0ced8b08df1
mrbubbles-src <feaver5683@gmail.com>
2023-09-08 12:14:00 +0200

    update to new api calls

--- frontend/src/components/UserStatistic/UserStatistic.jsx
+++ frontend/src/components/UserStatistic/UserStatistic.jsx
@@ -1,18 +1,5 @@
-import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import Cookies from "js-cookie";
+import React from "react";
 import "./userstatistic.scss";
-
-function getPluralText(count, singularText, pluralText) {
-    if (count === 1) {
-        return `${count} ${singularText}`;
-    } else if (count > 1) {
-        return `${count} ${pluralText}`;
-    } else {
-        return `Kein ${singularText}`;
-    }
-}
-// nur /images/dateiname im backend abspeichern
 const UserStatistic = ({
     wantToRead,
     currentlyReading,
@@ -20,32 +7,43 @@ const UserStatistic = ({
     username,
     profileImage,
 }) => {
+    const serverURL = "http://localhost:3000";
+    const Avatar = `${serverURL}${profileImage}`;
     return (
-        <div>
-            {/* <h1>{readListData.title}</h1> */}
-            <p>
-                {getPluralText(
-                    alreadyRead.length,
-                    "bereits gelesenes Buch",
-                    "bereits gelesene Bücher"
-                )}
-            </p>
-            <p>
-                {getPluralText(
-                    currentlyReading.length,
-                    "aktuell lesendes Buch",
-                    "aktuell lesende Bücher"
-                )}
-            </p>
-            <p>
-                {getPluralText(
-                    wantToRead.length,
-                    "gewünschtes Buch",
-                    "gewünschte Bücher"
-                )}
-            </p>
-            {/* <img src={Avatar} alt="avatar" width={"100px"} /> */}
-        </div>
+        <>
+            <div className="user-statistic-profile-image-container">
+                <img src={Avatar} alt="avatar" width={"100px"} />
+            </div>
+            <article className="user-statistic-rank">
+                <h3 className="user-statistic-username">{username}</h3>
+            </article>
+            <article className="user-statistic-book-info-container">
+                <p className="user-statistic-book-info-already-read">
+                    <span className="user-statistic-already-read-count">
+                        {alreadyRead.length}
+                    </span>{" "}
+                    {alreadyRead.length === 1
+                        ? "bereits gelesenes Buch"
+                        : "bereits gelesene Bücher"}
+                </p>
+                <p className="user-statistic-book-info-currently-reading">
+                    <span className="user-statistic-currently-reading-count">
+                        {currentlyReading.length}
+                    </span>{" "}
+                    {currentlyReading.length === 1
+                        ? "aktuell lesendes Buch"
+                        : "aktuell lesende Bücher"}
+                </p>
+                <p className="user-statistic-book-info-want-to-tread">
+                    <span className="user-statistic-want-to-read-count">
+                        {wantToRead.length}
+                    </span>{" "}
+                    {wantToRead.length === 1
+                        ? "gewünschtes Buch"
+                        : "gewünschte Bücher"}
+                </p>
+            </article>
+        </>
     );
 };
 
