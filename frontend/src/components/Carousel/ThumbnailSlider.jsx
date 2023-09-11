87dd097a3b25c45dcb3643b0bbff9b976e8a6e86
metin <metinmete.arsu@dci.education>
2023-09-11 12:13:22 +0200

    Fixed: Deleted Spinner & Context because of issues | Continue: Link to Book Page

--- frontend/src/components/Carousel/ThumbnailSlider.jsx
+++ frontend/src/components/Carousel/ThumbnailSlider.jsx
@@ -1,8 +1,6 @@
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { BookNookContext } from "../../context/BookNookProvider";
-
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import NoImage from "../../../public/images/various/no-image.png";
@@ -13,43 +11,38 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 const ThumbnailSlider = ({ slides }) => {
-    const { isLoading } = useContext(BookNookContext);
-    if (isLoading) {
-        return <LoadingSpinner />;
-    }
-    if (isLoading)
-        return (
-            <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={0}
-                slidesPerView={4}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSlideChange={() => console.log("slide change")}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                {slides.map((slide, index) => {
-                    const { smallThumbnail, medium } =
-                        slide.bookdetails.volumeInfo.imageLinks || {};
-                    const { title } = slide.bookdetails.volumeInfo;
-                    return (
-                        <SwiperSlide key={index}>
-                            <div className='thumbnail-slide-container'>
-                                <img
-                                    src={
-                                        smallThumbnail ||
-                                        medium?.replace("http", "https") ||
-                                        NoImage
-                                    }
-                                    alt={title}
-                                />{" "}
-                            </div>
-                        </SwiperSlide>
-                    );
-                })}
-            </Swiper>
-        );
+    return (
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={0}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {slides.map((slide, index) => {
+                const { smallThumbnail, medium } =
+                    slide.bookdetails.volumeInfo.imageLinks || {};
+                const { title } = slide.bookdetails.volumeInfo;
+                return (
+                    <SwiperSlide key={index}>
+                        <div className='thumbnail-slide-container'>
+                            <img
+                                src={
+                                    smallThumbnail ||
+                                    medium?.replace("http", "https") ||
+                                    NoImage
+                                }
+                                alt={title}
+                            />{" "}
+                        </div>
+                    </SwiperSlide>
+                );
+            })}
+        </Swiper>
+    );
 };
 
 export default ThumbnailSlider;
