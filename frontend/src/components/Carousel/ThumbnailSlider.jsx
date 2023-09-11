6c9fede1789ae0c8ae76ba2f2745bfe03c514573
metin <metinmete.arsu@dci.education>
2023-09-11 11:26:09 +0200

    Added: Carousel for Already Read & Want To Read / Created Thumbnail Slider component

---
+++ frontend/src/components/Carousel/ThumbnailSlider.jsx
@@ -0,0 +1,48 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import React, { useState, useContext } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import NoImage from "../../../public/images/various/no-image.png";
+import Modal from "../Modal/Modal";
+import { BookNookContext } from "../../context/BookNookProvider";
+import "./carousel.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const ThumbnailSlider = ({ slides }) => {
+    return (
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={0}
+            slidesPerView={4}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {slides.map((slide, index) => {
+                const { smallThumbnail, medium } =
+                    slide.bookdetails.volumeInfo.imageLinks || {};
+                const { title } = slide.bookdetails.volumeInfo;
+                return (
+                    <SwiperSlide key={index}>
+                        <div className='thumbnail-slide-container'>
+                            <img
+                                src={
+                                    smallThumbnail ||
+                                    medium?.replace("http", "https") ||
+                                    NoImage
+                                }
+                                alt={title}
+                            />{" "}
+                        </div>
+                    </SwiperSlide>
+                );
+            })}
+        </Swiper>
+    );
+};
+
+export default ThumbnailSlider;
