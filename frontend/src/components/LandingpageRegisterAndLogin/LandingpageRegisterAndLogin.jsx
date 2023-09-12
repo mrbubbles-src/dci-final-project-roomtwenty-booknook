78609a8ec9150118e27db847bcc8e99a6f098109
metin <metinmete.arsu@dci.education>
2023-09-12 10:09:11 +0200

    Changes: Text leicht geändert | Added: Button für Login & Register Modal

--- frontend/src/components/LandingpageRegisterAndLogin/LandingpageRegisterAndLogin.jsx
+++ frontend/src/components/LandingpageRegisterAndLogin/LandingpageRegisterAndLogin.jsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BookNookContext } from "../../context/BookNookProvider";
+import { Link } from "react-router-dom";
+import Modal from "../Modal/Modal";
+import LoginForm from "../LoginForm/LoginForm";
+import SignupForm from "../SignupForm/SignupForm";
 import "./LandingpageRegisterAndLogin.scss";
 
 const LandingpageRegisterAndLogin = () => {
+    const {
+        isLoggedIn,
+        setIsLoggedIn,
+        showModal,
+        handleCloseModal,
+        handleLoginClick,
+        handleLogout,
+        isLoginVisible,
+        toggleForm,
+    } = useContext(BookNookContext);
     return (
-        <div className='text-container'>
-            <p className='text intro'>
-                Wenn Du alle unsere Features nutzen möchtest registriere dich
-                doch bei uns und lege eigene Bücherlisten an, vernetze dich mit
-                gleichgesinnten und vieles mehr!
-            </p>
-
-            <p className='text register'>
-                Zur Registration geht es <a href='/#'>hier entlang</a>.
+        <article className='text-container'>
+            <p className='text login'>
+                Wenn du alle unsere Features nutzen möchtest, registriere dich
+                doch bei uns und lege sowie verwalte deine eigenen Bücherlisten.
             </p>
             <p className='text login'>
-                Bist du bereits Mitglied? Dann log dich <a href='/#'>hier</a>{" "}
-                ein!
+                Erstelle eigene Challenges um am Ball zu bleiben.
             </p>
-        </div>
+            <p className='text login'>
+                Wohin die Reise geht, ist ganz allein Deine Entscheidung !
+            </p>
+            <button className='btn-login' onClick={handleLoginClick}>
+                Hier ist dein Ticket
+            </button>
+            {showModal && (
+                <Modal onClose={handleCloseModal}>
+                    {isLoginVisible ? (
+                        <LoginForm
+                            onClose={handleCloseModal}
+                            onLogin={() => setIsLoggedIn(true)}
+                        />
+                    ) : (
+                        <SignupForm
+                            onClose={handleCloseModal}
+                            onLogin={() => setIsLoggedIn(true)}
+                        />
+                    )}
+                    <button className='btn-login' onClick={toggleForm}>
+                        {isLoginVisible
+                            ? "Noch kein Mitglied?"
+                            : "Bereits Mitglied?"}
+                    </button>
+                </Modal>
+            )}
+        </article>
     );
 };
 
