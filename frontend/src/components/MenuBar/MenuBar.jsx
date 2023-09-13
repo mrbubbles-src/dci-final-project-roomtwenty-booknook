c81e3770b64f4140e3ebc6bcebd2477b129d6fb5
metin <metinmete.arsu@dci.education>
2023-09-13 09:10:06 +0200

    Changes: Alles vereinfacht ohne Logik

--- frontend/src/components/MenuBar/MenuBar.jsx
+++ frontend/src/components/MenuBar/MenuBar.jsx
@@ -21,44 +21,105 @@ const MenuBar = () => {
     };
 
     return (
-        <div className='menubar'>
+        <div className='menu'>
             {isLoggedIn && (
                 <>
-                    <div className='menu-item inactive effect'>
-                        <div className='menu-item-icon'>
-                            {" "}
-                            <FontAwesomeIcon icon={faCalendarDays} />
-                        </div>
-                        <div className='menu-item-text'>Calendar</div>
-                    </div>
-                    <div className='menu-item inactive effect'>
-                        <div className='menu-item-icon'>
-                            {" "}
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        </div>
-                        <div className='menu-item-text'>Search</div>
-                    </div>
-                    <div className='menu-item active effect'>
-                        <div className='menu-item-icon'>
-                            {" "}
-                            <FontAwesomeIcon icon={faUser} />
-                        </div>
-                        <div className='menu-item-text'>Profile</div>
-                    </div>
-                    <div className='menu-item inactive effect'>
-                        <div className='menu-item-icon'>
-                            {" "}
-                            <FontAwesomeIcon icon={faBook} />
-                        </div>
-                        <div className='menu-item-text'>Books</div>
-                    </div>
-                    <div className='menu-item inactive effect'>
-                        <div className='menu-item-icon'>
-                            {" "}
-                            <FontAwesomeIcon icon={faGear} />
-                        </div>
-                        <div className='menu-item-text'>Settings</div>
-                    </div>
+                    {" "}
+                    {/* DEAD BUTTON */}
+                    <NavLink
+                        className='nav'
+                        to='/#'
+                        onClick={() =>
+                            handleIconClick(
+                                "Diese Funktion ist derzeitig in der Entwicklung"
+                            )
+                        }
+                    >
+                        <div className='menu-item inactive accent'>
+                            <div className='menu-item-icon'>
+                                {" "}
+                                {
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faCalendarDays}
+                                    />
+                                }
+                            </div>
+                            <div className='menu-item-text'>Calendar</div>
+                        </div>{" "}
+                    </NavLink>{" "}
+                    {/* SEARCH */}
+                    <NavLink className='nav' to='suche'>
+                        <div className='menu-item active'>
+                            <div className='menu-item-icon'>
+                                {" "}
+                                {
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faMagnifyingGlass}
+                                    />
+                                }
+                            </div>
+                            <div className='menu-item-text'>Search</div>
+                        </div>{" "}
+                    </NavLink>{" "}
+                    {/* USER */}
+                    <NavLink to='/#'>
+                        <div className='menu-item active'>
+                            <div className='menu-item-icon'>
+                                {" "}
+                                {
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faUser}
+                                    />
+                                }
+                            </div>
+                            <div className='menu-item-text'>Profile</div>
+                        </div>{" "}
+                    </NavLink>{" "}
+                    {/* DEAD BUTTON */}
+                    <NavLink
+                        className='nav'
+                        to='/#'
+                        onClick={() =>
+                            handleIconClick("Bücher (In Entwicklung)")
+                        }
+                    >
+                        <div className='menu-item'>
+                            <div className='menu-item-icon'>
+                                {" "}
+                                {
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faBook}
+                                    />
+                                }
+                            </div>
+                            <div className='menu-item-text'>Books</div>
+                        </div>{" "}
+                    </NavLink>{" "}
+                    {/* DEAD BUTTON */}
+                    <NavLink
+                        className='nav'
+                        to='/#'
+                        onClick={() =>
+                            handleIconClick("Einstellungen (In Entwicklung)")
+                        }
+                    >
+                        <div className='menu-item'>
+                            <div className='menu-item-icon'>
+                                {" "}
+                                {
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faGear}
+                                    />
+                                }
+                            </div>
+                            <div className='menu-item-text'>Settings</div>
+                        </div>{" "}
+                    </NavLink>
                 </>
             )}
             <ToastContainer position='top-right' autoClose={2000} />
