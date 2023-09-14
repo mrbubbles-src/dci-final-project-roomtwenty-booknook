422dee11fac65bad45664d844b9d72d80148f9be
metin <metinmete.arsu@dci.education>
2023-09-14 12:14:50 +0200

    Changes: Ready! Just the Button left

--- frontend/src/components/LandingpageRegisterAndLogin/LandingpageRegisterAndLogin.jsx
+++ frontend/src/components/LandingpageRegisterAndLogin/LandingpageRegisterAndLogin.jsx
@@ -9,24 +9,8 @@ const LandingpageRegisterAndLogin = () => {
     const { handleLoginClick } = useContext(BookNookContext);
     return (
         <>
-            <hr />
             {/* TABLET & DESKTOP */}
             <section className='text-container-tablet'>
-                <article className='article-container'>
-                    <div className='article-container-text'>
-                        <h2 className='article-heading'>Bücherlisten</h2>
-                        <p className='article-text'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Sapiente fuga exercitationem sequi a quae ut
-                            reprehenderit necessitatibus nulla, et non.
-                        </p>
-                    </div>
-                    <img
-                        className='landingpage-article-image'
-                        src={image1}
-                        alt=''
-                    />
-                </article>
                 <article className='article-container'>
                     <div className='article-container-text'>
                         <h2 className='article-heading'>Challenges</h2>
@@ -39,18 +23,39 @@ const LandingpageRegisterAndLogin = () => {
                     </div>
                     <img
                         className='landingpage-article-image'
+                        src={image1}
+                        alt=''
+                    />
+                </article>
+                <article className='article-container'>
+                    <div className='article-container-text-left'>
+                        <h2 className='article-heading-left'>Bücherlisten</h2>
+                        <p className='article-text-left'>
+                            Du kannst deine Bücher in verschiedenen Listen
+                            speichern und sie nach deinen eigenen Vorlieben
+                            organisieren und verwalten. Diese individuellen
+                            Listen ermöglichen es dir, deine Leseerfahrungen zu
+                            strukturieren und Bücher auf eine Weise zu
+                            kategorisieren, die deinen Bedürfnissen und
+                            Interessen entspricht.
+                        </p>
+                    </div>
+                    <img
+                        className='landingpage-article-image-left'
                         src={image2}
                         alt=''
                     />
                 </article>
                 <article className='article-container'>
                     <div className='article-container-text'>
-                        <h2 className='article-heading'>Du entscheidest</h2>
+                        <h2 className='article-heading'>Deine Entscheidung!</h2>
                         <p className='article-text'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Consequatur officiis nihil quibusdam
-                            exercitationem facere ducimus sint corporis debitis,
-                            recusandae perferendis?
+                            Bei deiner Reise durch die Welt der Bücher liegt die
+                            Entscheidung ganz bei dir, egal welches Genre oder
+                            Tempo du bevorzugst. Du bestimmst, wohin diese
+                            literarische Reise führt, und kannst Bücher in
+                            deinem eigenen Tempo und nach deinem eigenen
+                            Geschmack erkunden.
                         </p>
                     </div>
                     <img
@@ -59,7 +64,7 @@ const LandingpageRegisterAndLogin = () => {
                         alt=''
                     />
                 </article>
-                <button className='btn-login' onClick={handleLoginClick}>
+                <button className='btn-ticket' onClick={handleLoginClick}>
                     Hier ist dein Ticket!
                 </button>
             </section>
