b69ec8ff6309f294d7dd6f4ebecb591b9cf904d3
katharina-groller <Katharina.Groller@dci.education>
2023-09-15 09:12:09 +0200

    changes

--- frontend/src/components/LevelExpBar/LevelExpBar.jsx
+++ frontend/src/components/LevelExpBar/LevelExpBar.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
-import "./LevelExpBar.scss";
 
 const LevelExpBar = ({ xpProzent }) => {
     return (
         <div className="expLevel">
             <ProgressBar
+                className="progressBar"
                 completed={xpProzent}
                 bgColor="#fcd8be"
                 baseBgColor="#554945"
                 borderRadius="20px"
                 labelAlignment="center"
-                labelSize="2.8rem"
+                labelSize="1.3rem"
                 labelColor="#37323e"
-                height="5rem"
-                className="progressBar"
+                height="3rem"
                 animateOnRender={true}
                 isLabelVisible={true}
             />
