b44cae391cbf7adceb05724143134603e5f5ba37
katharina-groller <Katharina.Groller@dci.education>
2023-09-17 18:13:44 +0200

    richtigen code wieder eingefü. nach commit in main

--- frontend/src/components/LevelExpBar/LevelExpBar.jsx
+++ frontend/src/components/LevelExpBar/LevelExpBar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
+import "./LevelExpBar.scss";
+import "../UserStatistic/userstatistic.scss";
 
 const LevelExpBar = ({ xpProzent }) => {
     return (
         <div className="expLevel">
             <ProgressBar
-                className="progressBar"
-                completed={xpProzent}
                 bgColor="#fcd8be"
                 baseBgColor="#554945"
-                borderRadius="20px"
+                className="wrapper"
                 labelAlignment="center"
-                labelSize="1.3rem"
-                labelColor="#37323e"
-                height="3rem"
+                barContainerClassName="container"
+                //completedClassName="barCompleted"
+                labelClassName="label"
+                completed={xpProzent}
                 animateOnRender={true}
                 isLabelVisible={true}
             />
