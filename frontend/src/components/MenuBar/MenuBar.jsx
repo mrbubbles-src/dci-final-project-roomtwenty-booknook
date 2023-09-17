f95b9198fc07a3d20c2e458fca5487e6d4b4e241
metin <metinmete.arsu@dci.education>
2023-09-17 15:53:50 +0200

    Here we go

--- frontend/src/components/MenuBar/MenuBar.jsx
+++ frontend/src/components/MenuBar/MenuBar.jsx
@@ -6,89 +6,120 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faMagnifyingGlass,
     faUser,
-    faCalendarDays,
+    faCompass,
+    faBars,
     faBook,
-    faGear,
 } from "@fortawesome/free-solid-svg-icons";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import MenuBarContent from "../MenuBarContent/MenuBarContent";
 
 const MenuBar = () => {
     const { isLoggedIn } = useContext(BookNookContext);
-    const [showContent, setShowContent] = useState(false);
+    const [isContentActive, setIsContentActive] = useState(false);
+
     const toolTipText = "Diese Funktion ist derzeitig in der Entwicklung";
+
     const handleIconClick = () => {
         toast.info(toolTipText, { autoClose: 2000 });
     };
 
     const handleExtraContentClick = () => {
-        setShowContent(!showContent);
+        setIsContentActive(!isContentActive);
     };
 
     return (
-        <div>
+        <div className={"menu-main-container"}>
             {isLoggedIn && (
-                <div className='menu'>
-                    {/* DEAD BUTTON */}
-                    <NavLink to='/#' onClick={() => handleIconClick()}>
-                        <div className='menu-item'>
-                            <div className='menu-item-icon'>
-                                <FontAwesomeIcon
-                                    className='menu-icon'
-                                    icon={faCalendarDays}
-                                />
+                <div className='menu-second-container'>
+                    {" "}
+                    <div
+                        className={`menu ${
+                            isContentActive ? "menu-expanded" : ""
+                        }`}
+                    >
+                        {/* DEAD BUTTON */}
+                        <NavLink to='/#' onClick={() => handleIconClick()}>
+                            <div className='menu-item'>
+                                <div className='menu-item-icon'>
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faCompass}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </NavLink>
-                    {/* SEARCH */}
-                    <NavLink to='suche'>
-                        <div className='menu-item active'>
-                            <div className='menu-item-icon'>
-                                <FontAwesomeIcon
-                                    className='menu-icon'
-                                    icon={faMagnifyingGlass}
-                                />
+                        </NavLink>
+                        {/* SEARCH */}
+                        <NavLink to='suche'>
+                            <div className='menu-item'>
+                                <div className='menu-item-icon'>
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faMagnifyingGlass}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </NavLink>
-                    {/* USER */}
-                    <NavLink to='/#'>
-                        <div className='menu-item'>
-                            <div className='menu-item-icon'>
-                                <FontAwesomeIcon
-                                    className='menu-icon'
-                                    icon={faUser}
-                                />
+                        </NavLink>
+                        {/* USER */}
+                        <NavLink to='/#'>
+                            <div className='menu-item'>
+                                <div className='menu-item-icon'>
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faUser}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </NavLink>
-                    {/* DEAD BUTTON */}
-                    <NavLink to='/#' onClick={() => handleIconClick()}>
-                        <div className='menu-item'>
-                            <div className='menu-item-icon'>
-                                <FontAwesomeIcon
-                                    className='menu-icon'
-                                    icon={faBook}
-                                />
+                        </NavLink>{" "}
+                        <NavLink to='/#' onClick={() => handleIconClick()}>
+                            <div className='menu-item'>
+                                <div className='menu-item-icon'>
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faBook}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </NavLink>
-                    {/* DEAD BUTTON */}
-                    <NavLink to='/#' onClick={handleExtraContentClick}>
-                        <div className='menu-item'>
-                            <div className='menu-item-icon'>
-                                <FontAwesomeIcon
-                                    className='menu-icon'
-                                    icon={faGear}
-                                />
+                        </NavLink>
+                        <NavLink to='/#' onClick={handleExtraContentClick}>
+                            <div className='menu-item'>
+                                <div className='menu-item-icon'>
+                                    <FontAwesomeIcon
+                                        className='menu-icon'
+                                        icon={faBars}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </NavLink>
+                        </NavLink>
+                        {isContentActive && (
+                            <div className='flex-in-grid'>
+                                {" "}
+                                <NavLink
+                                    to={"./About"}
+                                    className='footer-links'
+                                >
+                                    Über Uns
+                                </NavLink>
+                                <NavLink
+                                    to={"./Impressum"}
+                                    className='footer-links'
+                                >
+                                    Impressum
+                                </NavLink>
+                                <NavLink
+                                    to={"./Datenschutz"}
+                                    className='footer-links'
+                                >
+                                    Datenschutz
+                                </NavLink>
+                                <NavLink to={"#"} className='footer-links'>
+                                    Kontakt
+                                </NavLink>
+                            </div>
+                        )}
+                    </div>{" "}
                 </div>
             )}
             <ToastContainer position='top-right' autoClose={2000} />
-            {showContent ? <MenuBarContent /> : ""}
         </div>
     );
 };
