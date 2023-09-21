4b8d3070d258dd8ac261b768597b2938b92fcbff
mrbubbles-src <feaver5683@gmail.com>
2023-09-21 13:11:12 +0200

    frontend link cors

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,11 @@ mongoose
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: [
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "https://roomtwenty-booknook.vercel.app",
+        ],
         credentials: true,
     })
 );
