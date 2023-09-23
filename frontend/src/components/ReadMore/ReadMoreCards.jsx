8d5aaff0853aa6f1be0a1f5bbe3dd27f43a08346
mrbubbles-src <feaver5683@gmail.com>
2023-09-23 19:07:26 +0200

    created for userprofile cards

--- frontend/src/components/ReadMore/ReadMoreCards.jsx
+++ frontend/src/components/ReadMore/ReadMoreCards.jsx
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ReadMoreCards = ({ children, singlePageID }) => {
     const text = children;
-    const [isReadMore, setIsReadMore] = useState(true);
-    // const toggleReadMore = () => {
-    //     setIsReadMore(!isReadMore);
-    // };
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            const newWindowSize = {
+                width: window.innerWidth,
+                height: window.innerHeight,
+            };
+            setWindowSize(newWindowSize);
+        };
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    let displayText;
+    if (windowSize.width <= 768) {
+        displayText = text.slice(0, 90); // mobile
+    } else if (windowSize.width <= 1024) {
+        displayText = text.slice(0, 120); // tablet
+    } else {
+        displayText = text.slice(0, 400); // desktop
+    }
+
     return (
         <>
-            {isReadMore ? text.slice(0, 100) : text}
+            {displayText}
             <Link to={`/buch/${singlePageID}`} className="read-or-hide">
-                <strong>
-                    {isReadMore ? "...mehr lesen" : "...weniger lesen"}
-                </strong>
+                <strong>"...mehr lesen"</strong>
             </Link>
         </>
     );
